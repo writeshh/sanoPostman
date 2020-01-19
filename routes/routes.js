7516360627e616556e287a7485622afcd822778a
ErKiran <kiruu1238@gmail.com>
2020-01-19 16:27:37 +0545

    Error message checked

--- routes/routes.js
+++ routes/routes.js
@@ -1,44 +1,47 @@
-const {Router} = require('express');
-const {sanoPostman} = require('../controllers/apiExecutor');
-const {validateResponse} = require('../validations/responseValidator');
+const { Router } = require('express');
+const { sanoPostman } = require('../controllers/apiExecutor');
+const { validateResponse } = require('../validations/responseValidator');
 const router = Router();
 
-router.use('/sanoPostman', async(req,res)=>{
-const {body:{url}, headers, method, body, query} = req;
-if(!url){
-    throw new Error(`Url is required for making request`)
-}
+router.use('/sanoPostman', async (req, res) => {
+  const {
+    body: { url },
+    headers,
+    method,
+    body,
+    query
+  } = req;
+  if (!url) {
+    throw new Error(`Url is required for making request`);
+  }
 
-if(!method){
-    throw new Error(`Method is required for making request`)
-}
-const request = {};  
+  if (!method) {
+    throw new Error(`Method is required for making request`);
+  }
+  const request = {};
 
-if(headers){
-    request.headers = headers
-}
+  if (headers) {
+    request.headers = headers;
+  }
 
-if(query){
-    request.query = query
-}
+  if (query) {
+    request.query = query;
+  }
 
-if(body){
-    request.body = body;
-}
-const ree = await sanoPostman(method,url,request);
-const check = validateResponse(ree)
-if(check){
-if(check.status === 609){
-    return res.json(check)
-}
-}
+  request.body = body;
+  const ree = await sanoPostman(method, url, request);
+  const check = validateResponse(ree);
+  if (check) {
+    if (check.status === 609) {
+      return res.json(check);
+    }
+  }
 
-if(ree.status === 200){
-    return res.json(ree)
-}
+  if (ree.status === 200) {
+    return res.json(ree);
+  }
 
+  return res.status(ree.status).json(ree.msg);
+});
 
-return res.status(ree.status).json(ree.msg)
-})
-
-module.exports = router
+module.exports = router;
