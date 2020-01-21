d2ca49c73dc787720ad1e2e5acb6444c39ecee0e
writeshh <es.ritesh.shrestha@gmail.com>
2020-01-21 15:04:40 +0545

    updated validation error  messages

--- validations/responseValidator.js
+++ validations/responseValidator.js
@@ -1,52 +1,44 @@
 const StatusCodes = require('../library/statusCodes');
-const message_types = [
-  'text',
-  'image',
-  'video',
-  'audio',
-  'file',
-  'cards',
-  'list'
-];
+const message_types = ['text', 'image', 'video', 'audio', 'file', 'cards', 'list'];
 function validateResponse(res) {
-  try {
-    if (res.data) {
-      if (!res.data.version) {
-        return StatusCodes.validationError(
-          `Invalid Payload message Version is required`,
-          res
-        );
-      }
-      if (res.data.version !== 'v2') {
-        return StatusCodes.validationError(`Invalid Version`, res);
-      }
-      if (res.data.content) {
-        if (res.data.content.messages) {
-          if (!Array.isArray(res.data.content.messages)) {
-            if (!res.data.content.messages[0].type) {
-              return StatusCodes.validationError(`Type is required`, res);
-            }
-            const check = message_types.includes(
-              res.data.content.messages[0].type
-            );
-            if (!check) {
-              return StatusCodes.validationError(
-                `Unsupported message Type`,
-                res
-              );
-            }
-            return StatusCodes.validationError(`Message should be array`, res);
-          }
-          return StatusCodes.validationError(`Message is required`, res);
-        }
-        return StatusCodes.validationError(`Content is required`, res);
-      }
-    }
-  } catch (err) {
-    throw new Error(`Error while validating the response ${err}`);
-  }
+	try {
+		if (res.data) {
+			if (!res.data.version) {
+				return StatusCodes.validationError(`Invalid Payload message Version is required`, res);
+			}
+
+			if (res.data.version !== 'v2') {
+				return StatusCodes.validationError(`Invalid Version`, res);
+			}
+			if (res.data.content) {
+				if (res.data.content.messages) {
+					if (Array.isArray(res.data.content.messages)) {
+						if (!res.data.content.messages[0].type) {
+							return StatusCodes.validationError(`Type is required`, res);
+						}
+						const check = message_types.includes(res.data.content.messages[0].type);
+						if (!check) {
+							return StatusCodes.validationError(`Unsupported message Type`, res);
+						}
+					} else {
+						return StatusCodes.validationError(`Message should be array`, res);
+					}
+				} else {
+					return StatusCodes.validationError(`Message is required`, res);
+				}
+			} else {
+				return StatusCodes.validationError(`Content is required`, res);
+			}
+		}
+	} catch (err) {
+		console.log(`Error while validating the response ${err}`);
+		return res.json({
+			status: 500,
+			msg: 'Server Error',
+		});
+	}
 }
 
 module.exports = {
-  validateResponse
+	validateResponse,
 };
