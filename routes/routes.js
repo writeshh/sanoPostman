d26e02c96520c893bab9b1e6ca042ee95313dd5e
writeshh <es.ritesh.shrestha@gmail.com>
2020-01-21 09:47:58 +0545

    response return if url and method not found

--- routes/routes.js
+++ routes/routes.js
@@ -4,40 +4,49 @@ const { validateResponse } = require('../validations/responseValidator');
 const router = Router();
 
 router.use('/sanoPostman', async (req, res) => {
-  const {
-    body: { url },
-    headers,
-    method,
-    body,
-    query
-  } = req;
-  console.log(req.body)
-  if (!url) {
-    throw new Error(`Url is required for making request`);
-  }
+	const {
+		body: { url },
+		headers,
+		method,
+		body,
+		query,
+	} = req;
+	console.log(req.body);
 
-  if (!method) {
-    throw new Error(`Method is required for making request`);
-  }
-  const request = {};
+	if (!url) {
+		console.log('"Error - " URL not found.');
+		return res.json({
+			status: 400,
+			msg: 'URL is required to make a successful request.',
+		});
+	}
 
-  if (headers) {
-    request.headers = headers;
-  }
+	if (!method) {
+		console.log('"Error - " Method not found.');
+		return res.json({
+			status: 400,
+			msg: 'Method type is required to make a successful request.',
+		});
+	}
+	const request = {};
 
-  if (query) {
-    request.query = query;
-  }
+	if (headers) {
+		request.headers = headers;
+	}
 
-  request.body = body;
-  const ree = await sanoPostman(method, url, request);
-  const check = validateResponse(ree);
-  if (check) {
-    if (check.status === 609) {
-      return res.json(check);
-    }
-  }
-  return res.status(ree.status).json(ree);
+	if (query) {
+		request.query = query;
+	}
+
+	request.body = body;
+	const ree = await sanoPostman(method, url, request);
+	const check = validateResponse(ree);
+	if (check) {
+		if (check.status === 609) {
+			return res.json(check);
+		}
+	}
+	return res.status(ree.status).json(ree);
 });
 
 module.exports = router;
